c3f12a8e3357dcbcebad482270649bf32dc19534
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-11 22:39:26 +0300

    Added menu items to the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,22 @@
+import logo from '../../assets/logo.svg'
+
 const Navbar = () => {
     return (
-        <div className="Navbar">
-            Navbar
+        <div className="Navbar w-full z-30 sticky top-0 px-12 py-12 md:px-0 md:container md:mx-auto">
+            <div className='flex flex-row justify-between'>
+                <div className='flex flex-row justify-between'>
+                    <div>
+                        <img src={logo} alt="" className='mt-1 w-[62.56px] h-[16.02px] mr-14'/>
+                    </div>
+                    <div className="hidden lg:flex space-x-8 align-top text-lg">
+                        <a href="/#" className="hover:text-darkGrayishBlueLight">Home</a>
+                        <a href="/#" className="hover:text-darkGrayishBlueLight">What Is GPT3?</a>
+                        <a href="/#" className="hover:text-darkGrayishBlueLight">Open AI</a>
+                        <a href="/#" className="hover:text-darkGrayishBlueLight">Case Studies</a>
+                        <a href="/#" className="hover:text-darkGrayishBlueLight">Library</a>
+                    </div>
+                </div>
+            </div>            
         </div>
     )
 }
