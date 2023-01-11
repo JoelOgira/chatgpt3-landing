58c1862ea80a156b8f42cb11a2ea096e68ca328d
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-11 22:19:11 +0300

    Added menu items to the navbar

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,9 @@
+import logo from '../../assets/logo.svg';
+
 const Header = () => {
     return (
-        <div className="Header">
-            Header
+        <div className="Header flex flex-row justify-between space-x-4">
+            
         </div>
     )
 }
