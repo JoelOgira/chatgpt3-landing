7e71739fafe53d49bd37319c3e93037107be317b
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-15 21:01:44 +0300

    Added functionality to the WhatGPT component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,9 @@
 const WhatGPT3 = () => {
     return (
-        <div className="WhatGPT3">
-
+        <div className=" mt-16 px-8">
+            <div className="WhatGPT flex flex-row mx-auto md:container bg-colorBlog py-8">
+                
+            </div>
         </div>
     )
 }
