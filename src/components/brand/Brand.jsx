4e3e360f1766447d62da2420764a5ae7228271b9
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-15 20:59:29 +0300

    Changed styling in the brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -6,7 +6,7 @@ import shopify from '../../assets/shopify.png';
 
 const Brand = () => {
     return (
-        <div className="Brand flex grow overflow-y-auto flex-wrap justify-center items-center px-20 my-16">
+        <div className="Brand flex grow overflow-y-auto flex-wrap justify-center items-center px-18 md:px-24 mt-16">
             <div className='flex flex-1 justify-center items-center m-4 max-w-[150px] min-w-[120px]'>
                 <img src={google} />
             </div>
