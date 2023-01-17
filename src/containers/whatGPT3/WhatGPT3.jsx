978ee79a06bacfe1828bdcf329660a9c21978fe4
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-17 21:13:59 +0300

    Made whatgpt component functional

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,42 @@
+import Feature from "../../components/feature/Feature";
+
 const WhatGPT3 = () => {
     return (
-        <div className=" mt-16 px-8">
-            <div className="WhatGPT flex flex-row mx-auto md:container bg-colorBlog py-8">
-                
+        <div className="mt-16 px-8 mb-20">
+            <div className="WhatGPT flex flex-col mx-auto flex-wrap md:container bg-colorBlog p-8">
+                <Feature
+                    title="What is GPT-3"
+                    text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+                />
+
+                <div className="flex flex-col py-6 space-y-5 justify-between sm:flex-row sm:items-center ">
+                    <div className="flex">
+                        <h1 className="text-[28px] font-bold ss:text-[38px] max-w-[510px]">
+                            The possibilities are beyond your imagination
+                        </h1>
+                    </div>
+                    <div className="flex">
+                        <p className="text-colorSubtext">
+                            Explore the library
+                        </p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col justify-between pt-8 px-4">
+                    <Feature
+                        title="Chatbots"
+                        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+                    />
+                    <Feature
+                        title="Knowledgebase"
+                        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"
+                    />
+                    <Feature
+                        title="Education"
+                        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments"
+                    />                      
+                </div>
+
             </div>
         </div>
     )
