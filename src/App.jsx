4695216c9520c898b4c6bf96baf9c021dfc28e96
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-17 21:07:16 +0300

    Added new components for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
 
-import { Article, Brand, CTA, Feature, Navbar } from "./components";
+import { Article, Brand, CTA, Navbar } from "./components";
 
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./containers";
 
 const App = () => {
   return (
     <div className="App bg-colorBg text-white scroll-smooth">
-      <div className="bg-gradientBar md:px-8">
+      <div className="bg-gradientBar">
         <Navbar />
         <Header />
       </div>
       <Brand />
       <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
       <Footer />
       
     </div>
