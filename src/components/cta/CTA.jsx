3e0a4e390c8b9c816dc5fa24529e497f8cc18f8c
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-18 21:46:38 +0300

    Changed styling

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,22 +1,21 @@
 const CTA = () => {
     return (
         <div className="CTA px-8 mb-20">
-            <div className="flex flex-col mx-auto bg-white px-8 py-4 rounded-2xl md:container text-colorBlog">
-                <div className="flex flex-wrap flex-col py-6 space-y-8 justify-between sm:flex-row sm:items-center ">
-                    <div className="flex flex-col space-y-2 font-semibold text-[12px]">
+            <div className="flex flex-col mx-auto bg-gradient-to-r from-[#AE67FA] to-[#F49867] px-8 py-4 rounded-2xl md:container">
+                <div className="flex flex-col py-6 space-y-6 justify-between ss:flex-row ss:items-center ss:space-x-3 ss:space-y-0">
+                    <div className="flex flex-col space-y-2 font-semibold text-[12px] w-full ss:w-3/4">
                         <p>
                             Request Early Access to Get Started
                         </p>
-                        <h1 className="text-[22px] font-bold ss:text-[34px]">
+                        <h1 className="text-[22px] font-bold ss:text-[28px] ">
                             Register today & start exploring the endless possibilities.
                         </h1>
                     </div>
-                    <div className="flex mx-auto md:mx-0">
-                        <p className="text-white">
-                            <button className="bg-black rounded-full py-2 px-4 cursor-pointer sm:mb-2">
-                                Get Started
-                            </button>
-                        </p>
+                    
+                    <div className="flex mx-auto md:mx-0">                        
+                        <button className="bg-black rounded-full py-2 px-6 cursor-pointer sm:mb-2">
+                            Get Started
+                        </button>
                     </div>
                 </div>
             </div>
