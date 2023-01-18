66852634d518e2d1d54d97e09f0330cbfd2d0cdf
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-18 21:48:06 +0300

    Added gradient text to the h1 tag

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,7 +2,7 @@ const Feature = ({ title, text }) => {
     return (
         <div className="Feature flex flex-col pb-8 space-y-3 justify-between ss:flex-row ss:space-y-0 ">
             <div className="flex-1">
-                <div className="mb-1 w-[38px] h-[3px] bg-white">
+                <div className="mb-1 w-[38px] h-[3px] bg-gradient-to-r from-[#AE67FA] to-[#F49867]">
 
                 </div> 
                 <h1 className="text-[16px] ss:text-[18px] font-bold">
