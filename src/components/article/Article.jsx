9c3b370a5317610ff1de62c224006d781e713270
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-19 23:25:16 +0300

    Made article file functional

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,7 +1,16 @@
-const Article = () => {
+const Article = ( { imgUrl, date, text }) => {
     return (
-        <div className="Article">
-
+        <div className="Article  flex flex-col space-y-6 overflow-hidden w-full h-full bg-colorFooter">
+            <div className="w-full h-full bg-colorBg">
+                <img src={imgUrl} alt="blog image" className="w-full h-full" loading="lazy"/>
+            </div>
+            <div className="flex flex-col justify-between space-y-12 h-full px-6 pb-12">
+                <div>
+                    <p className="text-[11.65px] font-bold pb-3">{date}</p>
+                    <h3 className="text-[18px] font-extrabold ss:text-[25.11px]">{text}</h3>
+                </div>
+                <p className="text-[11.65px] font-bold">Read Full Article</p>
+            </div>           
         </div>
     )
 }
