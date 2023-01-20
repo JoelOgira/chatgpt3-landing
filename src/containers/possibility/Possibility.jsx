f8bd9648ea0c0f04fd9a3c658d298193903170dc
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-20 06:39:11 +0300

    Added lazy loading to images

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -6,7 +6,7 @@ const Possibility = () => {
             <div className="flex flex-col space-y-8 md:container mx-auto sm:flex-row sm:space-x-16">
 
                 <div className="flex flex-1 justify-start items-center sm:w-1/2">
-                    <img src={possibility} alt="Very possible image" className='w-full h-full' />
+                    <img src={possibility} loading="lazy" alt="Very possible image" className='w-full h-full' />
                 </div>
 
                 <div className="flex flex-col space-y-7 sm:w-1/2 sm:pt-[25%]">
@@ -14,7 +14,7 @@ const Possibility = () => {
                         Request Early Access and Get Started
                     </h4>
 
-                    <h1 className='text-[36px] sm:text-[46px] font-bold'>
+                    <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-[#AE67FA] to-[#F49867] text-[36px] sm:text-[46px] font-bold'>
                         The possibilities are beyond your imagination
                     </h1>
                     <p className='text-[16px] sm:text-[18px] text-colorText mt-6'>
