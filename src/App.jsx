eba4366669a97d5df22ac22910f0c9a1d233062f
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-01-30 21:00:44 +0300

    Changed styling in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from "./contain
 const App = () => {
   return (
     <div className="App bg-colorBg text-white scroll-smooth">
-      <div className="bg-gradientBar">
+      <div className="bg-gradientBar bg-gradient-to-r from-[rgba(0, 40, 83, 1)] to-[rgba(4, 12, 24, 1)]">
         <Navbar />
         <Header />
       </div>
